8bec0875c3e0c03bc0449f8399b5261a662b7bb5
nima321 <niman8900@gmail.com>
2023-04-11 00:39:25 +0100

    nimas comit

--- src/test/java/Group/GroupPractice.java
+++ src/test/java/Group/GroupPractice.java
@@ -5,5 +5,9 @@ import org.w3c.dom.ls.LSOutput;
 public class GroupPractice {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        for (int i = 0; i <=5 ; i++) {
+            System.out.println(i);
+
+        }
     }
 }
