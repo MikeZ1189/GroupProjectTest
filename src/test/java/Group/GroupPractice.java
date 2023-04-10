bf879c3005f03800461447070920130197b184e9
MikeZ1189 <zmich89@vt.edu>
2023-04-10 19:39:20 -0400

    michael commit

--- src/test/java/Group/GroupPractice.java
+++ src/test/java/Group/GroupPractice.java
@@ -4,6 +4,8 @@ import org.w3c.dom.ls.LSOutput;
 
 public class GroupPractice {
     public static void main(String[] args) {
+
         System.out.println("Hello world");
+        System.out.println("This is Michael");
     }
 }
